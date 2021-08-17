49655de9e4cc6de1010ec9cfcc6e37c8e236ca61
wayfrr <wayfrr@gmail.com>
2021-08-17 18:14:50 +0530

    Excluding vid #99,#100,upto #101,#98 about js may be removed

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, HttpResponse,redirect
 from blog.models import Post, BlogComment
 from django.contrib import messages
+from blog.templatetags import extras
 
 # Create your views here.
 def blogHome(request): 
@@ -11,8 +12,18 @@ def blogHome(request):
 
 def blogPost(request, slug):
     post = Post.objects.filter(slug=slug).first()
-    comments = BlogComment.objects.filter(post=post)
-    context = {'post':post, 'comments': comments}
+    post.views = post.views + 1
+    post.save()
+
+    comments = BlogComment.objects.filter(post=post,parent=None)
+    replies = BlogComment.objects.filter(post=post).exclude(parent=None)
+    replyDict ={}
+    for reply in replies:
+        if reply.parent.sno not in replyDict.keys():
+            replyDict[reply.parent.sno] = [reply]
+        else:
+            replyDict[reply.parent.sno].append(reply)
+    context = {'post':post, 'comments': comments,'user': request.user,'replyDict':replyDict}
     return render(request, 'blog/blogPost.html',context) 
     #return HttpResponse(f'This is blogPost : {slug}')
 
@@ -22,10 +33,18 @@ def postComment(request):
           user = request.user
           postSno = request.POST.get("postSno")
           post = Post.objects.get(sno=postSno)
+          parentSno = request.POST.get("parentSno")
 
-          comment = BlogComment(comment=comment, user=user, post=post)
-          comment.save()
-          messages.success(request, "Your comment has been posted successfully")
+          if parentSno=="":
+                comment = BlogComment(comment=comment,user=user, post=post)
+                comment.save()
+                messages.success(request, "Your comment has been posted successfully")
+          else:
+                parent = BlogComment.objects.get(sno=parentSno)
+                comment = BlogComment(comment=comment,user=user, post=post,parent=parent)
+          
+                comment.save()
+                messages.success(request, "Your reply has been posted successfully")
           
 
     return redirect(f"/blog/{post.slug}")
